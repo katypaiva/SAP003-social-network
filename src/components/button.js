0fe563656a53dfc59057a5ac9a5abb9d51624b6f
tamirissg <tamiris@icamiabas.com.br>
2019-10-21 13:59:22 -0300

    botão de excluir ok

--- src/components/button.js
+++ src/components/button.js
@@ -1,6 +1,6 @@
 function Button(props) {
   const template = `
-    <button class="${props.class}" onclick="button.handleClick(event, ${props.onclick})">${props.title}</button>
+    <button data-id=${props.dataId} class="${props.class}" onclick="button.handleClick(event, ${props.onclick})">${props.title}</button>
   `;
   return template;
 }
@@ -8,8 +8,9 @@ function Button(props) {
 window.button = {
   handleClick: (event, callback) => {
     event.preventDefault();
-    callback();
+    callback(event);
   },
+  component: Button,
 };
 
 
--- src/pages/feed.js
+++ src/pages/feed.js
@@ -3,64 +3,82 @@ import Textarea from '../components/textarea.js';
 
 
 function Feed(props) {
-loadPost()
-  const template =`
-    <form id ="formPost">
-    ${Textarea({ class: 'Text1', placeholder: ''})}
-    ${Button({ class: 'mytext', onclick:formPost, title: 'ENVIAR' })}
-    </form>
-    <ul id="posts"></ul>
-    `
-    return template;
-  }
-
-  //essa percorre os posts do template e carrega eles
-  function loadPost () {
-    const loading = document.querySelector('.loading');
-    loading.innerHTML=""
-    const user = firebase.auth().currentUser;
-     const collectionPost = firebase.firestore().collection('posts')
-    collectionPost.where('user', '==', user.uid).get().then(snap => {snap.forEach(post => {
-    addingPost(post)
-    }) 
-  }) 
-  }
-  
-// essa função cria o objeto do post no banco de dados e cria o card
-function formPost(){
-  const id = firebase.auth().currentUser.uid
-  const text = document.querySelector('.Text1').value;
-  const post = {
-    user: id,
-    likes : 0,
-    comments:[],
-    text: text,
-    time: new Date().toLocaleString('pt-BR'),
-  }
-  firebase.firestore().collection('posts').add(post)
-    .then(res => {
-      document.querySelector('#posts').innerHTML += `
-      <div class='card-post'>
-      ${post.text}
-      ${post.likes}
-      ${post.time}
-      </div>
+  loadPost()
+    const template =`
+      <form id ="formPost">
+      ${Textarea({ class: 'Text1', placeholder: ''})}
+      ${Button({ class: 'mytext', onclick:formPost, title: 'ENVIAR' })}
+      ${Button({ class: 'btn-logout', onclick:logOut, title: 'SAIR' })}
+      </form>
+      <ul id="posts"></ul>
       `
-    })
-}
+      return template;
+    }
   
-// esta busca os posts do banco de dados e adiciona no template
-function addingPost(post){
-  const listPost = document.querySelector('#posts');
-  const templatePost = `
-  <li>
-  ${post.data().text}
-  ${post.data().time}
-  ${post.data().likes}
-  </li>
-  `
-  listPost.innerHTML += templatePost
-}
+    //essa percorre os posts do template e carrega eles
+    function loadPost () {
+      const loading = document.querySelector('.loading');
+      loading.innerHTML=""
+      const user = firebase.auth().currentUser;
+     const collectionPost = firebase.firestore().collection('posts')
+      collectionPost.where('user', '==', user.uid).get().then(snap => {snap.forEach(post => {
+      addingPost(post)
+  
+      }) 
+    }) 
+    }
+    
+  // essa função cria o objeto do post no banco de dados e cria o card
+  function formPost(){
+    const id = firebase.auth().currentUser.uid;
+    const text = document.querySelector('.Text1').value;
+    const post = {
+      user: id,
+      likes : 0,
+      comments:[],
+      text: text,
+      time: new Date().toLocaleString('pt-BR'),
+    }
+    firebase.firestore().collection('posts').add(post)
+      .then(res => {
+        document.querySelector('#posts').innerHTML += `
+        <section class='card-post'>
+        <p class='post-text'>${post.text}</p>
+        <p class='likes'>${post.likes}</>
+        <p class='date-time'>${post.time}</p> 
+        </section>
+        `
+      })
+  }
+    
+  // esta busca os posts do banco de dados e adiciona no template
+  function addingPost(post){
+    const listPost = document.querySelector('#posts');
+    const templatePost = `
+    <section class='card-post'>
+    <div class='card-texts'>
+    <p class='post-text'>${post.data().text}</p>
+    <p class='likes'>${post.data().likes}
+    <p class='date-time'>${post.data().time}</p>
+    ${Button({ dataId: post.id, class: 'btn-delete', onclick:deletePost, title: 'EXCLUIR' })}
+    </div>
+    </section>
+    `
+    listPost.innerHTML += templatePost
+  }
+  
+  //Função de logout
+  function logOut () {
+    firebase.auth().signOut();
+  }
 
+  // função de deletar 
+  function deletePost (event){
+    const id = event.target.dataset.id;
+    firebase.firestore().collection('posts').doc(id).delete();
+    event.target.parentElement.remove();
+  };
+    
+  
 
-export default Feed;
+  export default Feed;
